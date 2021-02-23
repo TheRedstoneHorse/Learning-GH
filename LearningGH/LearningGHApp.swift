184bca43a85b538b68af7eebd568ebc23ebe76d0
Dhruv Dingari <ddingari@hotmail.com>
2021-02-23 09:15:41 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Dhruv Dingari on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
